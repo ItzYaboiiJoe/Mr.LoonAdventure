978139c85da26a95b352d5e99ca71fa7c084716c
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-11-27 23:30:32 -0500

    Player Movement Created

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,16 +1,45 @@
 using Godot;
 using System;
 
-public partial class PlayerMovement : Node
+public partial class PlayerMovement : CharacterBody2D
 {
-	
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public float Speed = 200.0f; // Character movement speed
+
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Vector2 velocity = Vector2.Zero;
+
+		// Input handling
+		if (Input.IsActionPressed("ui_right"))
+		{
+			velocity.X += 1;  // Correct casing for 'X'
+		}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.X -= 1;  // Correct casing for 'X'
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			velocity.Y += 1;  // Correct casing for 'Y'
+		}
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity.Y -= 1;  // Correct casing for 'Y'
+		}
+
+		// Normalize velocity to prevent faster diagonal movement
+		if (velocity != Vector2.Zero)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
+
+		// Move the character
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
